ef063944c9e8c3bc5b3dacb3ce306bd026642b00
JohannesUcel <u.johannes@hotmail.com>
2016-03-30 14:47:06 +0200

    zweite FUnktion

--- schokodb.php
+++ schokodb.php
@@ -48,7 +48,42 @@ function schokoSchnaeppchen_func() {
 	return ob_get_clean();
 }
 
+function schokoKunstschau_func() {
+	global $wpdb;
+	ob_start();
 
+?>
+	<form method="GET">
+		<select name="kunstschau">
+		<?php
+		$kunstschau = $wpdb->get_results("select * from Kunstschau");
+		foreach ($kunstschau as $k) {
+			echo '<option value="'.$k->name.'">'.
+				$k->name.
+				'</option>';
+		}
+		?>
+		</select>
+		<input type="submit" value="Anzeigen" class="btn btn-default">
+	</form>
+<?php
+        if (isset($_REQUEST['kunstschau'])) {
+                $KunstschauDat = $wpdb->get_results(
+		$wpdb->prepare('select bezeichnung, platz, name from zeigt inner join Produkt on zeigt.kunstwerknummer =
+				 Produkt.nummer where name = %s order by name',
+                $_REQUEST['kunstschau']));
+		echo '<table class="table table-striped table-hover"><h1> '.$KunstschauDat[0]->name.'<h1>';
+                echo '<tr><th><b>Kunstwerk</th> <th>Pl&auml;tze</th></tr>';
+		foreach ($KunstschauDat as $r) {			
+                        echo '<tr><td>'.$r->bezeichnung.'</td>';
+                        echo '<td>'.$r->platz.'</td></tr>';
+			}
+		echo '</table>';
+       		 }	
+	return ob_get_clean();
+
+}
 
 add_shortcode('schokoSchnaeppchen','schokoSchnaeppchen_func');
+add_shortcode('schokoKunstschau','schokoKunstschau_func');
 
